0a15cc23e87a5a59df678a9c8876fbd3043d42f3
Fernando Castellanos <fernando@billfloat.com>
2013-06-09 11:17:01 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Petproject::Application.config.secret_key_base = 'bc09543d8de351210e038736759f0795c69c976f4f4270c23ed751ac0df4793a6f49f9a7f0d5b504c2d841c12e06f64d89be24869fbd0dbd3c3b7ef6096be40a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Petproject::Application.config.session_store :cookie_store, key: '_petproject_session'
